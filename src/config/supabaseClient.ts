604910030d67c65d74494553fdef04728576a660
Jonathan Koh <jonathan.koh75@gmail.com>
2024-05-22 20:57:35 +0800

    Update supabaseClient.ts

--- src/config/supabaseClient.ts
+++ src/config/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 // const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 // const supabaseKey = import.meta.env.VITE_ANON_KEY
-const supabaseUrl = ${{ secrets.SUPABASE_URL }}
-const supabaseKey = ${{ secrets.SUPABASE_ANON_KEY }}
+const supabaseUrl = ${{ secrets.VITE_SUPABASE_URL }}
+const supabaseKey = ${{ secrets.VITE_ANON_KEY }}
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase

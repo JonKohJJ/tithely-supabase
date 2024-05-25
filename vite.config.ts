a7296a808b5e6603ce63ee55008c1bb0dbe2287f
Jonathan Koh <jonathan.koh75@gmail.com>
2024-05-25 20:06:52 +0800

    debugging deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/tithely-supabase/"
+  base: "/tithely-supabase/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
+  },
 })

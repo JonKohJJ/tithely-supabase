f663bb302a1c57c8adf11ba1da2381abd563cda7
Jonathan Koh <jonathan.koh75@gmail.com>
2024-05-28 14:23:05 +0800

    debugging deployment

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,13 @@
 import './Footer_Style.css'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
     <section className="footer-section flex justify-center py-[50px] mt-auto">
       <div className="mycontainer flex justify-center items-center gap-1">
         <p className="fs-caption">&copy; {new Date().getFullYear()} Tithely All Rights Reserved</p>
-        <p className="fs-base">∙</p>
-        <Link to='/style-guide' className="fs-caption">Style Guide</Link>
+        {/* <p className="fs-base">∙</p> */}
+        {/* <Link to='/style-guide' className="fs-caption">Style Guide</Link> */}
       </div>
     </section>
   )
